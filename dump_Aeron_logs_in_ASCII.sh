18cc5050f755aec7570c871af2a440d68faf01e6
Alexey Shananin <ashananin@gmail.com>
2018-11-12 17:58:09 +0300

    extract AERON_LOGS_BASE

--- dump_Aeron_logs_in_ASCII.sh
+++ dump_Aeron_logs_in_ASCII.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-AERON_LOGS_BASE=/dev/shm/aeron-alex
-
 for filename in ${AERON_LOGS_BASE}/publications/UDP*.logbuffer; do
     java -cp lib/agrona-0.9.26.jar:lib/aeron-client-1.11.4-SNAPSHOT.jar:lib/aeron-samples-1.11.4-SNAPSHOT.jar \
         -Daeron.log.inspector.data.format=ascii \
